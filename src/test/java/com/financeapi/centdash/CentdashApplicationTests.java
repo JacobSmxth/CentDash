97a6324f35f123a7a6542bca60b94df779c8d67e
Jacob Smith <jacob.d.smith@live.com>
2025-10-18 13:08:58 -0400

    Restructure project

---
+++ src/test/java/com/financeapi/centdash/CentdashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financeapi.centdash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentdashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
